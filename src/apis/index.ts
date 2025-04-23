4064c850f3ebbea1821e157b009296ed916857a8
knowave <kwaktaemin98@gmail.com>
2025-04-23 15:43:07 +0900

    BASE URL env 변수처리

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,63 +1,57 @@
-import axios from "axios";
-import { IsJwtExpired } from "../utils/is-jwt-expired";
+import axios from 'axios';
+import { IsJwtExpired } from '../utils/is-jwt-expired';
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = process.env.BASE_URL;
 
 export const api = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
+    baseURL: BASE_URL,
+    withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-  if (config.headers.requiresAuth) {
-    const token = localStorage.getItem("accessToken");
+    if (config.headers.requiresAuth) {
+        const token = localStorage.getItem('accessToken');
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
     }
-  }
 
-  return config;
+    return config;
 });
 
 api.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    const { config, response } = error;
+    (res) => res,
+    async (error) => {
+        const { config, response } = error;
 
-    if (response?.status === 401 && !config._retry) {
-      config._retry = true;
+        if (response?.status === 401 && !config._retry) {
+            config._retry = true;
 
-      const refreshToken = localStorage.getItem("refreshToken");
+            const refreshToken = localStorage.getItem('refreshToken');
 
-      if (!refreshToken || IsJwtExpired(refreshToken)) {
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
-        window.location.href = "/account/signin";
+            if (!refreshToken || IsJwtExpired(refreshToken)) {
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+                window.location.href = '/account/signin';
+                return Promise.reject(error);
+            }
+
+            try {
+                const res = await axios.post('/auth/refresh', {}, { withCredentials: true });
+                const newAccessToken = res.data.accessToken;
+
+                localStorage.setItem('accessToken', newAccessToken);
+                api.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+                return api(config);
+            } catch (refreshError) {
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+                window.location.href = '/account/signin';
+                return Promise.reject(refreshError);
+            }
+        }
+
         return Promise.reject(error);
-      }
-
-      try {
-        const res = await axios.post(
-          "/auth/refresh",
-          {},
-          { withCredentials: true }
-        );
-        const newAccessToken = res.data.accessToken;
-
-        localStorage.setItem("accessToken", newAccessToken);
-        api.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${newAccessToken}`;
-        return api(config);
-      } catch (refreshError) {
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
-        window.location.href = "/account/signin";
-        return Promise.reject(refreshError);
-      }
     }
-
-    return Promise.reject(error);
-  }
 );
